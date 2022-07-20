38ca81478525967cffe392ad97041701251702bc
Chaitanya Bhilange <107292155+ChaitanyaBhilange@users.noreply.github.com>
2022-07-19 20:20:18 -0700

    Add files via upload

---
+++ [Days]reverse order programm.py
@@ -0,0 +1,9 @@
+#wpp to display the element  of a list in reverse order.
+
+days = ['sunday','monday','tuesday','wednesday','thursday']
+
+print('\nin reverse order:')
+i=len(days)-1
+while i>=0:
+    print(days[i])
+    i-=1
